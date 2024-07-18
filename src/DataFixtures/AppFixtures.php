bcd788aa081a724f4d8ab16be708956ef07863c1
Goldstein Ludivine <sneakymombox@gmail.com>
2024-07-18 15:01:00 +0200

    fixtures completed

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,20 +2,56 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
+use App\Entity\Customer;
 use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create();
 
+        // Create users
+        for ($i = 0; $i < 5; $i++) {
+            $user = new User();
+            $user->setEmail($faker->email);
+            $user->setRoles(['ROLE_USER']);
+
+            $hashedPassword = $this->passwordHasher->hashPassword($user, 'password');
+            $user->setPassword($hashedPassword);
+
+            $manager->persist($user);
+
+            // Create customers linked to the user
+            for ($j = 0; $j < 3; $j++) {
+                $customer = new Customer();
+                $customer->setName($faker->lastName);
+                $customer->setFirstName($faker->firstName);
+                $customer->setEmail($faker->email);
+                $customer->setCreatedAt($faker->dateTimeThisYear);
+                $customer->setPhoneNumber($faker->phoneNumber);
+                $customer->setAddress($faker->address);
+                $customer->setUser($user);
+
+                $manager->persist($customer);
+            }
+        }
+
+        // Create products
         for ($i = 0; $i < 20; $i++) {
             $product = new Product();
-            $product->setProductList($faker->word);
             $product->setName($faker->word);
             $product->setDescription($faker->sentence);
             $product->setBrand($faker->company);
